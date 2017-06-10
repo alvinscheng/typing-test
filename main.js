72ad19748c36c025af79e0413419f93ba0406b15
Alvin Cheng <ascheng21@gmail.com>
2017-06-10 08:11:53 -0700

    Changed keydown to keypress. #4

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 var $sentence = document.querySelector("#sentence")
-var sentence = 'Here is a random sentence to test how quickly and accurately you can type. Once you finish typing this sentence you will see a score.'
+var sentence = 'Test Sentence.'
+// 'Here is a random sentence to test how quickly and accurately you can type. Once you finish typing this sentence you will see a score.'
 
 for (var i = 0; i < sentence.length; i++) {
   $sentence.innerHTML += '<span>' + sentence[i] + '</span>'
 }
 
+var $first = document.querySelector('span')
 document.addEventListener('DOMContentLoaded', function(event) {
-  var $selected = document.querySelector('span')
-  $selected.classList.add('selected')
+  $first.classList.add('selected')
 })
 
 var $last = $sentence.lastChild
 var error = 0
 var $container = document.querySelector('.container')
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keypress', function(event) {
   if ($last.getAttribute('class') !== 'selected') {
     var $selected = document.querySelector('.selected')
     var $next = $selected.nextSibling
@@ -32,7 +33,7 @@ document.addEventListener('keydown', function(event) {
     if (event.key === $selected.textContent) {
       $selected.classList.remove('selected')
       $selected.classList.add('correct')
-      // Test complete! Show score
+      // Calculate time (onKeyPress?)
       var score = 'Test complete! You made ' + error + ' typos.'
       var $score = renderScore(score)
       $container.appendChild($score)
@@ -42,6 +43,10 @@ document.addEventListener('keydown', function(event) {
     }
   }
 })
+
+document.onkeypress = function(event) {
+
+}
 
 function renderScore(text) {
   var $score = document.createElement('p')
