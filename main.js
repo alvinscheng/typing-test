5718f9b539220396d99ada8a28b9cb291e96b3ea
Alvin Cheng <ascheng21@gmail.com>
2017-06-09 22:22:19 -0700

    Displays message with number of errors below. #4

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var $sentence = document.querySelector("#sentence")
-var sentence = 'Test Sentence.'
-// 'Here is a random sentence to test how quickly and accurately you can type. Once you finish typing this sentence you will see a score.'
+var sentence = 'Here is a random sentence to test how quickly and accurately you can type. Once you finish typing this sentence you will see a score.'
 
 for (var i = 0; i < sentence.length; i++) {
   $sentence.innerHTML += '<span>' + sentence[i] + '</span>'
@@ -13,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 var $last = $sentence.lastChild
 var error = 0
+var $container = document.querySelector('.container')
 document.addEventListener('keydown', function(event) {
   if ($last.getAttribute('class') !== 'selected') {
     var $selected = document.querySelector('.selected')
@@ -28,13 +28,23 @@ document.addEventListener('keydown', function(event) {
   }
   else if ($last.getAttribute('class') === 'selected') {
     var $selected = document.querySelector('.selected')
+    // Test is completed!
     if (event.key === $selected.textContent) {
       $selected.classList.remove('selected')
       $selected.classList.add('correct')
       // Test complete! Show score
+      var score = 'Test complete! You made ' + error + ' typos.'
+      var $score = renderScore(score)
+      $container.appendChild($score)
     }
     else {
       error++
     }
   }
 })
+
+function renderScore(text) {
+  var $score = document.createElement('p')
+  $score.textContent = text
+  return $score
+}
