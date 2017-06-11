607a39027111f30567fb8fdeb7170a118e92aa9b
Alvin Cheng <ascheng21@gmail.com>
2017-06-11 14:03:51 -0700

    Calculate time the test took to complete. #4

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var $sentence = document.querySelector("#sentence")
-var sentence = 'Test Sentence.'
-// 'Here is a random sentence to test how quickly and accurately you can type. Once you finish typing this sentence you will see a score.'
+var sentence = 'Here is a random sentence to test how quickly and accurately you can type. Once you finish typing this sentence you will see a score.'
 
 for (var i = 0; i < sentence.length; i++) {
   $sentence.innerHTML += '<span>' + sentence[i] + '</span>'
@@ -14,7 +13,11 @@ document.addEventListener('DOMContentLoaded', function(event) {
 var $last = $sentence.lastChild
 var error = 0
 var $container = document.querySelector('.container')
+var startTime, endTime, totalTime
 document.addEventListener('keypress', function(event) {
+  if (event.key === $first.textContent) {
+    startTime = new Date()
+  }
   if ($last.getAttribute('class') !== 'selected') {
     var $selected = document.querySelector('.selected')
     var $next = $selected.nextSibling
@@ -29,12 +32,13 @@ document.addEventListener('keypress', function(event) {
   }
   else if ($last.getAttribute('class') === 'selected') {
     var $selected = document.querySelector('.selected')
-    // Test is completed!
     if (event.key === $selected.textContent) {
+      // Test is completed!
       $selected.classList.remove('selected')
       $selected.classList.add('correct')
-      // Calculate time (onKeyPress?)
-      var score = 'Test complete! You made ' + error + ' typos.'
+      endTime = new Date()
+      totalTime = (endTime.getTime() - startTime.getTime()) / 1000
+      var score = 'Test complete! You took ' + totalTime + ' seconds and made ' + error + ' typos.'
       var $score = renderScore(score)
       $container.appendChild($score)
     }
@@ -43,10 +47,6 @@ document.addEventListener('keypress', function(event) {
     }
   }
 })
-
-document.onkeypress = function(event) {
-
-}
 
 function renderScore(text) {
   var $score = document.createElement('p')
